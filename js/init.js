1c1e2e1bf960a34fe63b76dd948b5e11b2b5d799
Etongxue <1341361765@qq.com>
2022-05-29 20:09:53 +0800

    520

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 /*
- * @Author: www.itab.link
+ * @Author: tsuki
  * @Date: 2022-01-07 22:32:28
- * @LastEditors: itab.link
+ * @LastEditors: tsuki
  * @LastEditTime: 2022-03-07 14:43:15
  * @Description: file content
  */
